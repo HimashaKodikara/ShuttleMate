499df1d3241d20c7d54b4e35ac3343943e5fe94e
19840769 <kodikarah.21@uom.lk>
2024-09-29 23:44:26 +0530

    updated firebase file

---
+++ src/contexts/authContext/index.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import {auth} from "../../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = React.createContext();
+
+export function AuthProvider({children}){
+    const[currentUser, setCurrentUser] = useState(null);
+    const[userLoggedIn, setUserLoggedIn] = useState(false);
+    const[loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const unsunscribe  = onAuthStateChanged(auth, initialzeUser);
+        return unsunscribe;
+    } , [])
+
+
+    async function initializeUser(user){
+        if(user){
+            setCurrentUser({...user});
+            setUserLoggedIn(true);
+        }
+        else{
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading
+    }
+}
+
