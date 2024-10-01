cb0ee5c57179038978805ee1a2aaa9638bde22cf
19840769 <kodikarah.21@uom.lk>
2024-10-01 22:09:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom'; // Import Navigate for redirection
 
 import { AuthProvider } from './context/AuthContext';
 import Login from './componenets/Login';
 import Register from './componenets/Register';
 import Home from './componenets/Home';
-
+import Splashscreen from './componenets/Splashscreen';
 
 function App() {
   return (
     <AuthProvider>
-    <div className="App">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home/>}/>
-      </Routes>
-    </div>
-  </AuthProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navigate to="/Splashscreen" />} /> {/* Redirect root to home */}
+          <Route path="/Splashscreen" element={<Splashscreen />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
