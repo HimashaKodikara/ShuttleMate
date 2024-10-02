6e7d07c3c0f74cdd5b2983e19cb6e3e1ea3d11f1
19840769 <kodikarah.21@uom.lk>
2024-10-02 12:17:28 +0530

    updated

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,11 +1,13 @@
 import Google from './google.png';
 import BackArrow from './backarrow.png';
 import Bg from './Bg.jpg'
+import player from './player.jpg';
 
 const images = {
     Google,
     BackArrow
-    ,Bg
+    ,Bg,
+    player
   };
   
   export default images; 
--- src/componenets/Home.jsx
+++ src/componenets/Home.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import images from '../assets/Images/index.js';
 
 const Home = () => {
-  const navigate = useNavigate();
+  
 
-  const handleClick = () => {
-    navigate('/login'); // Replace with your target route
-  };
+  const cards = [
+    { id: 1, title: 'Upload Video', image:images.player, link: '/card1' },
+    { id: 2, title: 'Card 2', image: 'https://via.placeholder.com/150', link: '/card2' },
+    { id: 3, title: 'Card 3', image: 'https://via.placeholder.com/150', link: '/card3' },
+    { id: 4, title: 'Card 4', image: 'https://via.placeholder.com/150', link: '/card4' },
+  ];
 
   return (
-    <div>
-      <button onClick={handleClick}>Go to New Page</button>
+    <div className='h-screen bg-slate-950 '>
+      <h1 className='pt-10 font-serif font-black text-center text-white text-7xl'>Welcome to Shuttlemate</h1>
+      <p className='mt-12 text-3xl text-center text-white '>Select a option</p>
+       <div className="grid grid-cols-1 gap-12 p-4 px-10 pt-14 sm:grid-cols-2 lg:grid-cols-4">
+      {cards.map(card => (
+        <Link key={card.id} to={card.link}>
+          <div className="overflow-hidden bg-white rounded-lg shadow-md cursor-pointer">
+            <img src={card.image} alt={card.title} className="object-cover w-full h-72" />
+            <div className="p-4">
+              <h3 className="text-lg font-bold text-center">{card.title}</h3>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
     </div>
   );
 };
