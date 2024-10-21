0147e88c7c47116ad7fc077b940e7ff4373978bf
19840769 <kodikarah.21@uom.lk>
2024-10-21 23:20:19 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Video from '../pages/VideoPage';
+import images from '../assets/Images';
 
 const Navbar = () => {
     return (
-        <nav className="p-4 bg-gray-800 shadow-sm shadow-white">
-            <div className="container flex items-center justify-between mx-auto">
-                <div className="pl-5 font-serif text-2xl text-white">Shuttlemate</div>
+        <nav className="p-1 bg-gray-800 shadow-sm shadow-white">
+            <div className="container flex items-center justify-between ">
+                <div className='flex flex-row items-center justify-center ml-10 space-x-16'><img src={images.logo} height={40} width={75}/>
+                <div className="items-center font-serif text-2xl font-bold text-white ">Shuttlemate</div>
+                </div>
                 <div className="hidden pr-8 space-x-6 md:flex">
                     <a href="Home" className="text-gray-300 hover:text-white">Home</a>
                     <a href="VideoPage" className="text-gray-300 hover:text-white">Videos</a>
